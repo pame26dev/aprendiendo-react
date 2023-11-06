170a8868bee2ff5f490ac82f954cfad802d26024
pame26dev <pame26112@gmail.com>
2023-11-06 14:55:42 -0300

    Add first class

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+
+const users = [
+  {
+    userName: 'pame26dev',
+    name: 'Pamela Opazo Hernández',
+    isFollowing : true
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Míguez',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Pablo Hdez',
+    isFollowing: true
+  }
+]
+
+export function App () {
+  //const format = (userName) => `@${userName}`
+
+  return (
+    <section className="App">
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        )
+        )
+      }
+    </section>
+  )
+}
