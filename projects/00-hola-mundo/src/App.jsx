4ced97613b56382f52c7f6b540b31320e0c375fd
pame26dev <pame26112@gmail.com>
2024-01-31 22:26:31 -0300

    Update extension app.jsx

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,42 +1,38 @@
-import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
-
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
 
 const users = [
   {
-    userName: 'pame26dev',
-    name: 'Pamela Opazo Hernández',
-    isFollowing : true
+    userName: "pame26dev",
+    name: "Pamela Opazo Hernández",
+    isFollowing: true,
   },
   {
-    userName: 'midudev',
-    name: 'Miguel Ángel Míguez',
-    isFollowing: false
+    userName: "midudev",
+    name: "Miguel Ángel Míguez",
+    isFollowing: false,
   },
   {
-    userName: 'PacoHdezs',
-    name: 'Pablo Hdez',
-    isFollowing: true
-  }
-]
+    userName: "PacoHdezs",
+    name: "Pablo Hdez",
+    isFollowing: true,
+  },
+];
 
-export function App () {
+export function App() {
   //const format = (userName) => `@${userName}`
 
   return (
     <section className="App">
-      {
-        users.map(({ userName, name, isFollowing }) => (
-          <TwitterFollowCard
-            key={userName}
-            userName={userName}
-            initialIsFollowing={isFollowing}
-          >
-            {name}
-          </TwitterFollowCard>
-        )
-        )
-      }
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
     </section>
-  )
+  );
 }
